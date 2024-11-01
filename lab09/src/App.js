30022d98fdd9486969144356271ddf4b47180791
thien5919 <thien5919tran@gmail.com>
2024-11-01 12:00:51 -0400

    lab 9 donegit add .!

---
+++ lab09/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <h3>Student Id: 101333237</h3>
+        <h3>Full name: Duc Thien Tran</h3>
+        <h4>George Brown College, Toronto</h4>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
